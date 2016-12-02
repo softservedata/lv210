8103c3064b631a4c6cac1eff7347134ae0188615
DmutroKot <dmutrokot94@gmail.com>
2016-12-02 12:40:45 +0200

    Add Homework1 Task1 and Task2 and deleted previous incorrect projects

---
+++ SoftServe/HomeWork1_Task1/HomeWork1_Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input variable a : ");
+            int a = int.Parse(Console.ReadLine());
+
+            int area = Area(a);
+
+            int perimetr = Perimetr(a);
+
+            Console.WriteLine("Area is equal : {0}, perimeter is equal : {1} with side length a : {2}", area, perimetr, a);
+
+            Console.ReadKey();
+        }
+
+        private static int Area(int a)
+        {
+            return a * a;
+        }
+
+        private static int Perimetr(int a)
+        {
+            return 4 * a;
+        }
+    }
+}
---
+++ SoftServe/HomeWork1_Task2/HomeWork1_Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How old are you, {0}?", name);
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Person name is : {0} and age is : {1}", name, age);
+
+            Console.ReadKey();
+        }
+    }
+}
