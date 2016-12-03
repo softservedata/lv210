9ab621b267fda28b031eacc262bc1d9aacad99e2
DmutroKot <dmutrokot94@gmail.com>
2016-12-03 11:27:19 +0200

    Add HomeWork2 Task4

---
+++ SoftServe/HomeWork2/HomeWork2_Task4/HomeWork2_Task4/Dog.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWork2_Task4
+{
+    public struct Dog
+    {
+        public string Name;
+        public string Mark;
+        public int Age;
+
+        public override string ToString()
+        {
+            return string.Format("Dog name is : {0}, dog mark is : {1}, dog age is : {2}", Name, Mark, Age);
+        }
+    }
+}
---
+++ SoftServe/HomeWork2/HomeWork2_Task4/HomeWork2_Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWork2_Task4
+{
+    class Program
+    {
+        /// <summary>
+        /// Task Description : declare struct Dog with fields Name, Mark, Age. 
+        /// Decleare variable myDog of Dog type and read values for it.
+        /// Output myDos into console.(Decleare method ToString in struct) 
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+
+            Console.Write("Input dog's name : ");
+            myDog.Name = Console.ReadLine();
+
+            Console.Write("Input dog's mark : ");
+            myDog.Mark = Console.ReadLine();
+
+            Console.Write("Input dog's age : ");
+            myDog.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(myDog);
+
+            Console.ReadKey();
+        }
+    }
+}
