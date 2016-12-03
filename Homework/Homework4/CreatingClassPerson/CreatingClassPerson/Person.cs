4fbb89b9bf998a8f927bea05c08c41d435eea4f7
uliana <ulyanaholub@gmail.com>
2016-12-03 19:05:20 +0200

    adding homework 4

---
+++ Homework/Homework4/CreatingClassPerson/CreatingClassPerson/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingClassPerson
+{
+    class Person
+    {
+        public string Name { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+
+        public Person(){}
+
+        public Person(string Name, DateTime Date)
+        {
+            this.Name = Name;
+            this.DateOfBirth = Date;
+        }
+
+        public int Age()
+        {
+            return DateTime.Now.Year - DateOfBirth.Year;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name - {0}, age - {1}", this.Name, this.Age());
+        }
+
+        public void ChangeName(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
---
+++ Homework/Homework4/CreatingClassPerson/CreatingClassPerson/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace CreatingClassPerson
+{
+ 
+    class Program
+    {
+
+        public static Person CreatePerson(string number)
+        {
+            Console.WriteLine("\nPerson {0}:", number);
+            Console.Write("\nName - ");
+            var inputName = Console.ReadLine();
+
+            Console.Write("Date of birth - ");
+            var inputDate = Console.ReadLine();
+            DateTime date; 
+
+            bool isValid = DateTime.TryParse(inputDate, out date);
+
+            if (isValid)
+                return new Person(inputName, date);
+            else
+            {
+                Console.WriteLine("Incorrect format of date. Person is not created.");
+                return null;
+
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            List<Person> PersonsList = new List<Person>();
+
+            Console.WriteLine("Please, enter information about five persons.");
+            Console.WriteLine("(Date of birth enter in format xx.xx.xxxx)");
+
+            for(int i=0; i<5; i++)
+            {
+                Person newPerson = CreatePerson((i + 1).ToString());
+                if (newPerson != null) PersonsList.Add(newPerson);
+            }
+
+            Console.WriteLine("\nInformation about all persons:\n");
+
+            for (int i = 0; i < PersonsList.Count; i++)
+            {
+                Console.WriteLine(PersonsList[i].ToString());
+            }
+
+
+            Console.WriteLine("\nChanged information about all persons:\n");
+            for (int i = 0; i < PersonsList.Count; i++)
+            {
+                    if (PersonsList[i].Age() < 16) PersonsList[i].ChangeName("Very young");
+                    Console.WriteLine(PersonsList[i].ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
