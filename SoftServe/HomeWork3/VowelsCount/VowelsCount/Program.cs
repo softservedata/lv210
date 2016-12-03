2cd3c2c4aa7bbcd1dc08bef57bd14cd0f0b5fa65
DmutroKot <dmutrokot94@gmail.com>
2016-12-03 12:16:15 +0200

    Add VowelsCount project

---
+++ SoftServe/HomeWork3/VowelsCount/VowelsCount/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input some text : ");
+            string text = Console.ReadLine();
+
+            char[] vowels = new char[] { 'a', 'o', 'i', 'e' };
+
+            int vowelsCount = text.Count(t => vowels.Contains(t));
+
+            Console.WriteLine("Total number of vowels are : {0}", vowelsCount);
+
+            Console.ReadKey();
+        }
+    }
+}
