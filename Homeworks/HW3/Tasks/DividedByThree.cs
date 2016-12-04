817d4ef62fe919744bbee98174723b3687bf1a2e
mdemchuk <mari.de.la.krua@gmail.com>
2016-12-04 22:16:22 +0200

    HW3 Presentstion tasks

---
+++ Homeworks/HW3/Tasks/DividedByThree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DividedByThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Input two integer numbers: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 3 == 0)
+                    count += 1;
+            }
+            Console.WriteLine("Amount of numbers which are divided into 3 entirely {0}", count);
+            Console.ReadLine();
+        }
+    }
+}
