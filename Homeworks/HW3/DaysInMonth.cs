24d1505b04468ce4aac5e349faedf6bd059881ec
mdemchuk <mari.de.la.krua@gmail.com>
2016-12-04 20:59:03 +0200

    Third homework

---
+++ Homeworks/HW3/DaysInMonth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DaysInMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number of month:");
+            int monthNumber = Convert.ToInt32((Console.ReadLine()));
+            int daysCount = DateTime.DaysInMonth(2016, monthNumber);
+            Console.WriteLine("Amount of days in this month: {0}", daysCount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homeworks/HW3/SumProduct.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SumProduct
+{
+    class Program
+    {
+        public static bool isPositive(int[] data)
+        {
+            bool isPositive = true;
+            for (int i = 0; i < 5; i++)
+            {
+                if (data[i] <= 0)
+                {
+                    isPositive = false;
+                    break;
+                }
+            }
+            return isPositive;
+        }
+        public static void Sum(int[] data)
+        {
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                sum += data[i];
+            }
+            Console.WriteLine("Sum of of first 5 elements: {0}", sum);
+        }
+        public static void Product(int[] data)
+        {
+            int prod = 1;
+            for (int i = data.Length - 1; i >= 5; i--)
+            {
+                prod *= data[i];
+            }
+            Console.WriteLine("Product of of last 5 elements: {0}", prod);
+        }
+        static void Main(string[] args)
+        {
+            int[] intData = new int[10];
+            Console.WriteLine("Input 10 integer numbers");
+            string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < (intData.Length < str.Length ? intData.Length : str.Length); ++i)
+                intData[i] = Convert.ToInt32(str[i]);
+            bool temp = isPositive(intData);
+            if (temp)
+            {
+                Sum(intData);
+            }
+            else
+                Product(intData);
+            Console.ReadLine();
+        }
+    }
+}
