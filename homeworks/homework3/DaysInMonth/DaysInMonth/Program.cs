c332df9ef5ee9cc88198c6af603b31667ae440ac
ROMSN2194 <lebedovichroman1@i.ua>
2016-12-05 12:47:48 +0200

    modify isMOnth()

--- homeworks/homework3/DaysInMonth/DaysInMonth/Program.cs
+++ homeworks/homework3/DaysInMonth/DaysInMonth/Program.cs
@@ -10,16 +10,17 @@ namespace DaysInMonth
     {
         public static bool isMonth(int MonthNumber)
         {
-            if (MonthNumber > 12 || MonthNumber <= 0) return false;
-            return true;
+            //if (MonthNumber > 12 || MonthNumber <= 0) return false;
+            return (MonthNumber <= 12 && MonthNumber > 0);
         }
+        
         static void Main(string[] args)
         {
             int []DaysPerMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
             Console.Write("Input month number:");
             int MonthNumber = Convert.ToInt32(Console.ReadLine());
             if (!isMonth(MonthNumber)) Console.WriteLine("Month number must be in range 1-12");
-            Console.WriteLine("Month {0} has {1} days", MonthNumber,DaysPerMonth[MonthNumber-1]);
+            else Console.WriteLine("Month {0} has {1} days", MonthNumber,DaysPerMonth[MonthNumber-1]);
             Console.ReadKey();
 
         }
