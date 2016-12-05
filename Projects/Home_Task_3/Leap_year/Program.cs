4c09976eaa7ba6d2535a37ec7817b2abcc306ea1
rostyslav.zhalivtsiv <rostyslav.zhalivtsiv@gmail.com>
2016-12-06 01:30:56 +0200

    home tasks3 and  mod2

---
+++ Projects/Home_Task_3/Leap_year/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap_year
+{
+    /// <summary>
+    /// Перевірити чи введений рік є високосним.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the year: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            bool b = i % 4 == 0;
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("{0} is leap year: {1}", i, b);
+        }
+    }
+}
