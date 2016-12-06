49656337e193524f9ba69fd3c1d09623db250464
yharasym <yharasym@STUDENT>
2016-12-06 17:23:18 +0200

    Add Class Numeric

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace Calc
 {
+    class Numeric
+    {
+
+        public double add(double arg0, double arg1)
+        {
+            return arg0 + arg0;
+        }
+
+        public double div(double arg0, double arg1)
+        {
+            return arg0 / arg1;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Numeric numeric = new Numeric();
+            double arg0;
+            double arg1;
+            //
+            Console.Write("arg0 = ");
+            Double.TryParse(Console.ReadLine(), out arg0);
+            //
+            Console.Write("arg0 = ");
+            Double.TryParse(Console.ReadLine(), out arg1);
+            //
+            Console.WriteLine("arg0 + arg1 = " + numeric.add(arg0, arg1));
+            Console.WriteLine("arg0 / arg1 = " + numeric.div(arg0, arg1));
         }
     }
 }
