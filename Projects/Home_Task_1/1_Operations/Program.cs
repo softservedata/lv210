5e699dc3491839766322e9ad1b1a06f88041f572
rostyslav.zhalivtsiv <rostyslav.zhalivtsiv@gmail.com>
2016-12-06 02:51:29 +0200

    1 Home Task

---
+++ Projects/Home_Task_1/1_Operations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_1
+{
+    /// <summary>
+    /// Define integer variables a and b.
+    /// Read values a and b from Console and calculate: a+b, a-b, a*b, a/b. 
+    /// Output obtained results.
+    /// </summary>
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Read Variables
+            Console.Write("Enter int number a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter int number b: ");
+            int b = Int32.Parse(Console.ReadLine());
+
+            //Result
+            Console.WriteLine("\n-----Result------");
+            Console.WriteLine("a + b = {0}", a + b);
+            Console.WriteLine("a - b = {0}", a - b);
+            Console.WriteLine("a * b = {0}", a * b);
+            Console.WriteLine("a / b = {0}", (double)a / b);
+
+        }
+    }
+}
