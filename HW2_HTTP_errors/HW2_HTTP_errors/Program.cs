ed8e5da1624446e0982985be7e1c3f17d103fe82
Orlann <orlann@ukr.net>
2016-12-06 12:46:32 +0200

    Add HW_HTTP_errosrs

---
+++ HW2_HTTP_errors/HW2_HTTP_errors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW2_HTTP_errors
+{
+    enum HTTPErrors : int { Bad_Request = 400, Unauthorized = 401, Forbidden = 403, Not_Found = 404, Method_Not_Allowed = 405, Not_Acceptable = 406,
+        Proxy_Authentication_Required = 407, Request_Timeout = 408, Conflict = 409, Gone = 410    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter mistake cod (betwin 400 and 410): ");
+            int mistake = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine((HTTPErrors)mistake);
+            Console.ReadKey();
+        }
+    }
+}
