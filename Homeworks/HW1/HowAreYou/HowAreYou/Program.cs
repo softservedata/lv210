978d23e03ee77d7d1fcdd6a4584208d93186a078
MariiaDemchuk <mari.de.la.krua@gmail.com>
2016-12-06 13:19:53 +0200

    commentary changed to <summary>

--- Homeworks/HW1/HowAreYou/HowAreYou/Program.cs
+++ Homeworks/HW1/HowAreYou/HowAreYou/Program.cs
@@ -4,11 +4,11 @@ namespace HowAreYou
 {
     class Program
     {
-	/*
-		Output question “How are you?“. 
-		Define string variable answer. 
-		Read the value answer and output: “You are (answer)". 
-	*/
+	///<summary>
+	///	Output question “How are you?“. 
+	///	Define string variable answer. 
+	///	Read the value answer and output: “You are (answer)". 
+	///</summary>
         static void Main(string[] args)
         {
             string answer;
