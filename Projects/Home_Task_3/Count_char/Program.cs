96ad2241dc32ee7f8ddddcd3fc89c1f7b9ac3654
rostyslav.zhalivtsiv <rostyslav.zhalivtsiv@gmail.com>
2016-12-06 02:57:08 +0200

    add 3 Home Task

---
+++ Projects/Home_Task_3/Count_char/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_char
+{
+    /// <summary>
+    /// Read the text as a string value and culculate the counts of characters 'a', 'o', 'i', 'e' in this text.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            int a_v = 0, o_v = 0, i_v = 0, e_v = 0;
+
+            Console.WriteLine("Type some text:");
+            string str = Console.ReadLine();
+
+            //loop
+            foreach (char s in str)
+            {
+                switch (s)
+                {
+                    case 'a':
+                    case 'A':
+                        a_v++;
+                        break;
+
+                    case 'o':
+                    case 'O':
+                        o_v++;
+                        break;
+
+                    case 'i':
+                    case 'I':
+                        i_v++;
+                        break;
+
+                    case 'e':
+                    case 'E':
+                        e_v++;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("\n----------RESULT----------");
+            Console.WriteLine("Char 'a-A' used {0} times", a_v);
+            Console.WriteLine("Char 'o-O' used {0} times", o_v);
+            Console.WriteLine("Char 'i-I' used {0} times", i_v);
+            Console.WriteLine("Char 'e-E' used {0} times", e_v);
+        }
+    }
+}
---
+++ Projects/Home_Task_3/Ten_int_numb/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ten_int_numb
+{
+    /// <summary>
+    /// Enter 10 integer numbers. 
+    /// Culculate the sum of first 5 elements if they are posetive or product of last 5 element in the other case.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            bool flag = true;
+            int sum = 0, prod = 1;
+            int[] arr1 = new int[5];
+            int[] arr2 = new int[5];
+
+            //Entering elements
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter {0} number: ", i + 1);
+
+                //For first 5 elements
+                if (i < 5)
+                {
+                    arr1[i] = Convert.ToInt32(Console.ReadLine());
+
+                    //Checking for negativ value
+                    if (arr1[i] < 0)
+                        flag = false;
+                }
+
+                //For last 5 elements
+                else
+                    arr2[i - 5] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //Cheking condition: If 'True' would be culc sum of first 5, either - product of last 5
+            if (flag)
+            {
+                foreach (int item in arr1)
+                {
+                    sum += item;
+                }
+
+                Console.WriteLine("\nSum of first 5 elements = {0}", sum);
+            }
+
+            else
+            {
+                foreach (int item in arr2)
+                {
+                    prod *= item;
+                }
+
+                Console.WriteLine("\nProduct of last 5 elements = {0}", prod);
+            }
+
+        }
+    }
+}
