a14a468116b9cf97b5d39bde8274deadcb5e973a
Orlann <orlann@ukr.net>
2016-12-06 15:28:46 +0200

    Task 2-2

---
+++ Pres2_Task2_1 (favourite color)/Pres2_Task2_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pres2_Task2_1
+{
+
+    class Program
+    {
+        enum Colors { Red, Blue, Green, Yellow };
+        static void Main(string[] args)
+        {
+            Colors myColor = Colors.Red;
+            Console.WriteLine("My favourite color is {0}.", myColor);
+            Console.ReadKey();
+        }
+    }
+}
