7ab874d36ce027c3bfdc37ada825c5b527c83a42
Orlann <orlann@ukr.net>
2016-12-06 12:55:19 +0200

    Add homework_dog

---
+++ Hw2_4 (Dog)/Hw2_dog/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw2_dog
+{
+    public struct Dog
+    {
+        public string Name;
+        public string Mark;
+        public string Age;
+        public string toString()
+        {
+            string r ="Dog has name "+ this.Name + " and mark " + this.Mark + " and age " + this.Age;
+            return r;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog;
+            Console.Write("Enter dog name: ");
+            myDog.Name = Console.ReadLine();
+            Console.Write("Enter dog mark: ");
+            myDog.Mark = Console.ReadLine();
+            Console.Write("Enter dog age: ");
+            myDog.Age = Console.ReadLine();
+            Console.WriteLine(myDog.toString());
+            Console.ReadKey();
+        }
+    }
+}
