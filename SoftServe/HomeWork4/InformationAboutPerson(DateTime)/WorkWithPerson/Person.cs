f142ec5d033ced0a530cf728140845cd8c4fe342
DmutroKot <dmutrokot94@gmail.com>
2016-12-08 14:37:22 +0200

    Add InformationAboutPerson with DateTime field

---
+++ SoftServe/HomeWork4/InformationAboutPerson(DateTime)/WorkWithPerson/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace HomeWork4_Task1
+{
+    class Person
+    {
+        private string name;
+        private DateTime birthDate;
+
+        public Person() { }
+
+        public Person(string _name, DateTime _birthDate)
+        {
+            name = _name;
+            birthDate = _birthDate;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get
+            {
+                return birthDate;
+            }
+        }
+
+        public int Age()
+        {
+            return DateTime.Now.Year - BirthDate.Year;
+        }
+
+        public static Person Input()
+        {
+            Console.WriteLine("Information about person.");
+            Console.Write("Input name for person: ");
+            var name = Console.ReadLine();
+
+            Console.Write("Input year of person's birth: ");
+            var date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            return new Person(name, date);
+        }
+
+        public void Output()
+        {
+            Console.WriteLine("Person's name: {0}, age: {1}", Name, Age());
+        }
+
+        public void ChangeName()
+        {
+            if (Age() < 16)
+            {
+                name = "Very Young";
+            }
+        }
+    }
+}
---
+++ SoftServe/HomeWork4/InformationAboutPerson(DateTime)/WorkWithPerson/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork4_Task1
+{
+    class Program
+    {
+        /// <summary>
+        /// Create Console Application project in VS.
+        /// Add class Person to the project.
+        /// Class Person should consists of
+        /// a) two private fields: name and birthDate field (full Date of birthday: day,month,year). Use DataTime type.
+        /// b) two properties for access to these fields(only get)
+        /// c) default constructor and constructor with 2 parameters
+        /// d) methods:
+        ///         Age - to calculate the age of person
+        ///         Input - to input information about person
+        ///         Output - to output information about person
+        ///         ChangeName - to change the name of person
+        /// In the method Main() create 5 objects of Person type and input information about them.
+        /// Then calculate and write to console the name and Age of each person;
+        /// Change the name of persons, which Age is less then 16, to "Very Young".
+        /// Output information about all persons.
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            var personCount = 1;
+            var persons = new List<Person>();
+
+            for (int i = 0; i < personCount; i++)
+            {
+                persons.Add(Person.Input());
+            }
+
+            persons.ForEach(p => p.Output());
+
+            persons.ForEach(p => p.ChangeName());
+
+            Console.WriteLine();
+
+            persons.ForEach(p => p.Output());
+
+            Console.ReadKey();
+        }
+    }
+}
