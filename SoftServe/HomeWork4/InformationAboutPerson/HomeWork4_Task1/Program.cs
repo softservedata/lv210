a9218dac4581e40b2289d7a85de23a02a6e9a5a8
DmutroKot <dmutrokot94@gmail.com>
2016-12-08 12:35:42 +0200

    Add folder Homework4

---
+++ SoftServe/HomeWork4/InformationAboutPerson/HomeWork4_Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personCount = 5;
+            var persons = new List<Person>();
+
+            for(int i = 0; i < personCount; i++)
+            {
+                persons.Add(Person.Input());
+            }
+
+            persons.ForEach(p => p.Output());
+
+            persons.ForEach(p => p.ChangeName());
+
+            Console.WriteLine();
+
+            persons.ForEach(p => p.Output());
+
+            Console.ReadKey();
+        }
+    }
+}
