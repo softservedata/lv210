f50e6de28068f382197ce84b13971db4af543e09
mdemchuk <mari.de.la.krua@gmail.com>
2016-12-09 19:34:05 +0200

    Add few metods

--- Homeworks/HW1/SimpleOperations/SimpleOperations/Program.cs
+++ Homeworks/HW1/SimpleOperations/SimpleOperations/Program.cs
@@ -4,24 +4,40 @@ namespace SimpleOperations
 {
     class Program
     {
-	///<summary>
-	///	Define integer variables a and b.
-	///	Read values a and b from Console and calculate: a+b, a-b, a*b, a/b. 
-	///	Output obtained results.
-	///</summary>
+        ///<summary>
+        ///	Define integer variables a and b.
+        ///	Read values a and b from Console and calculate: a+b, a-b, a*b, a/b. 
+        ///	Output obtained results.
+        ///</summary>
+        public static int Summ(int FirstNumber, int SecondNumber)
+        {
+            return FirstNumber + SecondNumber;
+        }
+        public static int Difference(int FirstNumber, int SecondNumber)
+        {
+            return FirstNumber - SecondNumber;
+        }
+        public static int Multiplax(int FirstNumber, int SecondNumber)
+        {
+            return FirstNumber * SecondNumber;
+        }
+        public static int Division(int FirstNumber, int SecondNumber)
+        {
+            return FirstNumber / SecondNumber;
+        }
         static void Main(string[] args)
         {
             int a, b;
             Console.WriteLine("Input integer values");
-            Console.WriteLine("a = ");
+            Console.Write("a = ");
             a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("b = ");
+            Console.Write("b = ");
             b = Convert.ToInt32(Console.ReadLine());
             int sum, diff, mult, div;
-            sum = a + b;
-            diff = a - b;
-            mult = a * b;
-            div = a / b;
+            sum = Summ(a, b);
+            diff = Difference(a, b);
+            mult = Multiplax(a, b);
+            div = Division(a, b);
             Console.WriteLine("a + b = {0};\na - b = {1};\na * b = {2};\na / b = {3};", sum, diff, mult, div);
             Console.ReadLine();
         }
