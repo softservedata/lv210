4f138ad57af8a9f1f1f294cf13b4f598c8d738fc
mdemchuk <mari.de.la.krua@gmail.com>
2016-12-10 18:21:41 +0200

    code refactoring

--- Homeworks/HW1/NameAndAge/NameAndAge/Program.cs
+++ Homeworks/HW1/NameAndAge/NameAndAge/Program.cs
@@ -4,21 +4,39 @@ namespace NameAndAge
 {
     class Program
     {
-	///<summary>
-	///	Define string variable name and integer value age. 
-	///	Output question "What is your name?";
-	///	Read the value name and output next question: "How old are you,(name)?". 
-	///	Read age and write whole information
-	///</summary>
+        ///<summary>
+        ///	Define string variable name and integer value age. 
+        ///	Output question "What is your name?";
+        ///	Read the value name and output next question: "How old are you,(name)?". 
+        ///	Read age and write whole information
+        ///</summary>
+        public static int ParseAtempt()
+        {
+            int ReadVariable;
+            bool ParseAtempt = Int32.TryParse(Console.ReadLine(), out ReadVariable);
+            if (ParseAtempt)
+            {
+                return ReadVariable;
+            }
+            else
+            {
+                throw new FormatException("Wrong data type.");
+            }
+        }
         static void Main(string[] args)
         {
-            string name;
-            int age;
-            Console.WriteLine("What is your name?");
-            name = Console.ReadLine();
-            Console.WriteLine("How old are you, {0}?", name);
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your name is {0} and you are {1} years old.", name, age);
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.Write("How old are you, {0}? ", name);
+            try
+            {
+                int age = ParseAtempt();
+                Console.WriteLine("Your name is {0} and you are {1} years old.", name, age);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
