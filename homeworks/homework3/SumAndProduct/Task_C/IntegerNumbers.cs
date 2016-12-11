e4a499e46db6cd42bcd76fc4d268d7a5441de0e8
ROMSN2194 <lebedovichroman1@i.ua>
2016-12-11 20:31:56 +0200

    Refactoring code

--- homeworks/homework3/SumAndProduct/Task_C/IntegerNumbers.cs
+++ homeworks/homework3/SumAndProduct/Task_C/IntegerNumbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,23 +12,23 @@ namespace Task_C
     {
         //Check if elements are positive.
         //countOfElements - number of elements to be checked
-        public static bool IsPositive(int []array,int countOfElements)
+        public static bool IsPositive(int[] array, int countOfElements)
         {
-          for (int i=0; i< countOfElements; i++)
+            for (int i = 0; i < countOfElements; i++)
             {
-                if (Array[i]<= 0)   return false;
-            }     
+                if (array[i] <= 0) return false;
+            }
             return true;
         }
 
         public static int CalcSumOfElements(int elementCount, int[] array)
         {
-            
+
             int sum = 0;
             for (int i = 0; i < elementCount; i++) sum += array[i];
             return sum;
         }
-        
+
         public static int CalcProductOfElements(int elementCount, int[] array)
         {
             int product = 1;
@@ -40,19 +40,20 @@ namespace Task_C
 
         static void Main(string[] args)
         {
-            
+
             Console.WriteLine("Input 10 integer numbers:");
-            int[] Array = new int[10];
-            for (int i = 0; i < Array.Length; i++)
+            string[] input = Console.ReadLine().Split(',');
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.Write("Number[{0}]=", i+1);
-                Array[i] = Convert.ToInt32(Console.ReadLine());
+
+                array[i] = Convert.ToInt32(input[i]);
             }
-            int numbersToCheck=5;
-            if (isPositive(Array,numbersToCheck))
-                Console.WriteLine("Sum of first 5 elements {0}", CalcSumOfElements(numbersToCheck, Array));
+            int numbersToCheck = 5;
+            if (IsPositive(array, numbersToCheck))
+                Console.WriteLine("Sum of first 5 elements {0}", CalcSumOfElements(numbersToCheck, array));
             else
-                Console.WriteLine("Product of last 5 elements {0}", CalcProductOfElements(numbersToCheck, Array));
+                Console.WriteLine("Product of last 5 elements {0}", CalcProductOfElements(numbersToCheck, array));
             Console.ReadKey();
         }
     }
