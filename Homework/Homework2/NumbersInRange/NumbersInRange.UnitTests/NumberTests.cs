ae9f757a728a22f426bd55d2cf8ebefd66ca404a
uliana <ulyanaholub@gmail.com>
2016-12-12 17:04:03 +0200

    adding some changes

--- Homework/Homework2/NumbersInRange/NumbersInRange.UnitTests/NumberTests.cs
+++ Homework/Homework2/NumbersInRange/NumbersInRange.UnitTests/NumberTests.cs
@@ -7,8 +7,12 @@ namespace NumbersInRange.UnitTests
     public class NumberTests
     {
         [Test]
-        [TestCase(-1, 1, 0, ExpectedResult = true)]
-        [TestCase(-1, 1, 3.5f, ExpectedResult = false)]
+        [TestCase(-5, 5, 0, ExpectedResult = true)]
+        [TestCase(-5, 5, 9.5f, ExpectedResult = false)]
+        [TestCase(-5, 5, 5, ExpectedResult = true)]
+        [TestCase(-5, 5, -5, ExpectedResult = true)]
+        [TestCase(-5, 5, -5.0001f, ExpectedResult = false)]
+        [TestCase(-5, 5, 5.0001f, ExpectedResult = false)]
         public bool Should_Return_True_When_Number_IsInRange(float a, float b, float value)
         {
             //Preconditions
