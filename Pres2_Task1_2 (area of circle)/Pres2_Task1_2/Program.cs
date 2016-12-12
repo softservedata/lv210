3f241e9f527e3e1996f6350e8aea27ab0b26bb68
orlann <orlann@ukr.net>
2016-12-12 13:13:31 +0200

    Add 3 methods

--- Pres2_Task1_2 (area of circle)/Pres2_Task1_2/Program.cs
+++ Pres2_Task1_2 (area of circle)/Pres2_Task1_2/Program.cs
@@ -4,15 +4,32 @@ namespace Pres2_Task1_2
 {
     class Program
     {
+        public static double CircleLength (double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public static double CircleArea (double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static double SphereVolume (double radius)
+        {
+            return 4 * Math.PI * Math.Pow(radius, 2);
+        }
+
         static void Main(string[] args)
         {
             Console.Write("Enter radius: ");
-            double r = Convert.ToDouble(Console.ReadLine());
-            double circleLength = 2*Math.PI*r;
-            double circleSquare = Math.PI * Math.Pow(r, 2);
-            double sphereVolume = 4*Math.PI* Math.Pow(r, 2);
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            double circleLength = CircleLength(radius);
+            double circleArea = CircleArea(radius);
+            double sphereVolume = SphereVolume(radius);
+
             Console.WriteLine("Length of circle = {0}", circleLength);
-            Console.WriteLine("Square of circle = {0}", circleSquare);
+            Console.WriteLine("Square of circle = {0}", circleArea);
             Console.WriteLine("Volume of sphere = {0}", sphereVolume);
             Console.ReadKey();
         }
