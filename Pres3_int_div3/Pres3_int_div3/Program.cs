d289b78df952b3e78963e4ffc5735e376ca2a234
Orlann <orlann@ukr.net>
2016-12-12 13:44:20 +0200

    Pres 3 int div 3

---
+++ Pres3_int_div3/Pres3_int_div3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pres3_int_div3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            Console.Write("Enter number 1: ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number 2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            for (int i=a; i<=b; i++)
+            {
+
+                if (i % 3 == 0)
+                {
+                    k = k+1;
+                }
+                Console.WriteLine("i={0}", i);
+                Console.WriteLine("k={0}", k);
+
+            }
+            Console.WriteLine("Quantity of number= {0}", k);
+            Console.ReadKey();
+
+        }
+    }
+}
