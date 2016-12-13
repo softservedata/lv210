60719e2d8df24fb9064f4dab05f70a8a05e56cb2
rfedytc <rostyslav.fedyk@gmail.com>
2016-12-13 14:07:36 +0200

    add unit tests

---
+++ HomeWork/HW3/SumOrProduct/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOrProduct
+{
+    public class Methods
+    {
+        public static bool IsPositive(int[] Arr, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (Arr[i] <= 0) return false;
+            }
+            return true;
+        }
+        public static int Sum(int[] Arr, int count)
+        {
+            int result = 0;
+            for (int i = 0; i < count; i++)
+            {
+                result += Arr[i];
+            }
+            return result;
+        }
+        public static int Product(int[] Arr, int count)
+        {
+            int product = 1;
+            for (int i = count; i < Arr.Length; i++)
+            {
+                product *= Arr[i];
+            }
+            return product;
+        }
+    }
+}
--- HomeWork/HW3/SumOrProduct/Program.cs
+++ HomeWork/HW3/SumOrProduct/Program.cs
@@ -19,6 +19,7 @@ namespace SumOrProduct
             Console.WriteLine("Enter 10 integer numbers:");
 
             int[] Arr = new int[10];
+            int count = 5;
 
             for (int i = 0; i < Arr.Length; i++)
             {
@@ -26,37 +27,18 @@ namespace SumOrProduct
                 Arr[i] = int.Parse(Console.ReadLine());
             }
 
-            if (IsPositive(Arr, 5))
-                Console.WriteLine("Sum of first 5 elements is {0}", Sum(Arr));
+            if (Methods.IsPositive(Arr, 5))
+            {
+                Console.WriteLine("Sum of first 5 elements is {0}", Methods.Sum(Arr, count));
+            }
             else
-                Console.WriteLine("Product of last 5 elements is {0}",Product(Arr));
-
+            {
+                Console.WriteLine("Product of last 5 elements is {0}", Methods.Product(Arr, count));
+            }
 
             Console.ReadKey();
         }
 
-        public static bool IsPositive(int[] Arr, int count)
-        {
-            for (int i = 0; i < count; i++)
-            {
-                if (Arr[i] <= 0) return false;
-            }
-            return true;
-        }
-
-        public static int Sum(int[] Arr)
-        {
-            int result = 0;
-            for (int i = 0; i < 5; i++) result += Arr[i];
-            return result;
-        }
-
-        public static int Product(int[] Arr)
-        {
-            int product = 1;
-            for (int i = 5; i < Arr.Length; i++)
-                product *= Arr[i];
-            return product;
-        }
+      
     }
 }
