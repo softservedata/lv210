e2cd238a13f185b77158149652e0e1e5ef2969ec
Orlann <orlann@ukr.net>
2016-12-13 15:25:24 +0200

    unit test for parity

--- Pres2_Task1_1 (parity)/Pres2_Task1/Program.cs
+++ Pres2_Task1_1 (parity)/Pres2_Task1/Program.cs
@@ -1,22 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pres2_Task1
 {
-    class Program
+    public class Program
     {
+        public static string ParityVerifying( double number1, double number2)
+        { 
+            return (number1 % 2 == 0) && (number2 % 2 == 0) || (number1 % 2 != 0) && (number2 % 2 != 0) ? "true" : "false";
+        }
+
         static void Main(string[] args)
         {
             Console.Write("Enter number 1: ");
-            int a = Convert.ToInt32(Console.ReadLine());
+            int number1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter number 2: ");
-            int b = Convert.ToInt32(Console.ReadLine());
-            string answer = (a % 2 == 0)&&(b % 2 ==0)||(a % 2!=0)&&(b % 2 !=0)?"true":"false" ;
-            string v = (answer == "true") ? "a and b have the same parity" : "a and b have different parity";
-            Console.WriteLine(v);
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            string answer = ParityVerifying(number1, number2);
+            string conclusion = (answer == "true") ? "a and b have the same parity" : "a and b have different parity";
+            Console.WriteLine(conclusion);
             Console.ReadKey();
         }
     }
---
+++ Pres2_Task1_1 (parity)/Pres2_Task1_1.UnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Pres2_Task1;
+
+namespace Pres2_Task1_1.UnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void ParityVerifyingTrueTest()
+        {
+            string ExcpectedResult = "true";
+            string ActualResult = Program.ParityVerifying(2, 4);
+            Assert.AreEqual(ExcpectedResult, ActualResult);
+        }
+
+        [Test]
+        public void ParityVerifyingFalseTest()
+        {
+            string ExcpectedResult = "false";
+            string ActualResult = Program.ParityVerifying(9, 4);
+            Assert.AreEqual(ExcpectedResult, ActualResult);
+        }
+    }
+}
