fca2e31e9e7db3eb93e3fd093e4a8482daa53e57
Orlann <orlann@ukr.net>
2016-12-13 14:12:00 +0200

    add HW4 and unit

---
+++ HW4_1/HW4_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] staff=new Person[5];
+            for(int i=0;i<5;i++)
+            {
+                staff[i]=new Person();
+                Console.WriteLine("Enter information about person{0}",i);
+                staff[i].Input();
+            }
+            for (int i = 0; i < 5;i++ )
+            {
+                staff[i].Output();
+                Console.WriteLine(staff[i].Name +" " + staff[i].Age());
+            }
+            for (int i = 0; i < 5;i++ )
+            {
+                if(staff[i].Age()<16)
+                {
+                    staff[i].ChangeName("Very young");
+                }
+                Console.WriteLine(staff[i].Name + " " + staff[i].Age());              
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Hw2_1 (-5 to 5)/ConsoleApplication1/Program.cs
+++ Hw2_1 (-5 to 5)/ConsoleApplication1/Program.cs
@@ -4,9 +4,8 @@ namespace HW2
 {
     class Program
     {
-        static void Main(string[] args)
+        void NumbersInRange()
         {
-           // Read 3 float numbers and check: are they all belong to the range[-5, 5]
             float a1, a2, a3;
             Console.Write("Enter numer 1: ");
             a1 = float.Parse(Console.ReadLine());
@@ -15,9 +14,15 @@ namespace HW2
             Console.Write("Enter numer 3: ");
             a3 = float.Parse(Console.ReadLine());
             Console.WriteLine();
-            String answer = ((a1 >= -5) && (a1 <= 5)) && ((a2 >= -5) && (a2 <= 5)) && ((a3 >= -5) && (a3 <= 5) )? "All number are between -5 and 5" : "Some number is not between -5 and 5";
+            String answer = ((a1 >= -5) && (a1 <= 5)) && ((a2 >= -5) && (a2 <= 5)) && ((a3 >= -5) && (a3 <= 5)) ? "All number are between -5 and 5" : "Some number is not between -5 and 5";
             Console.WriteLine(answer);
             Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.NumbersInRange();
         }
     }
 }
