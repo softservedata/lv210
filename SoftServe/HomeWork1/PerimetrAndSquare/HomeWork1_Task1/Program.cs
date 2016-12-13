f388c294290a2390aa41ce0186fbf0eb62009613
DmutroKot <dmutrokot94@gmail.com>
2016-12-13 13:37:44 +0200

    Add UnitTests

---
+++ SoftServe/HomeWork1/PerimetrAndSquare/HomeWork1_Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1_Task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input variable a : ");
+            int a = int.Parse(Console.ReadLine());
+
+            int area = SquareArea(a);
+
+            int perimetr = SquarePerimetr(a);
+
+            Console.WriteLine("Area of square is equal : {0}, perimeter is equal : {1} with length a : {2}", area, perimetr, a);
+
+            Console.ReadKey();
+        }
+
+        public static int SquareArea(int a)
+        {
+            return a * a;
+        }
+
+        public static int SquarePerimetr(int a)
+        {
+            return 4 * a;
+        }
+
+        public static void CheckInputedData(int sideLength)
+        {
+            if (sideLength < 0)
+            {
+                throw new ArgumentException("Input data are negative.");
+            }
+        }
+    }
+}
---
+++ SoftServe/HomeWork1/PerimetrAndSquare/PerimetrAndAreaOfSquare.UnitTests/UnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using HomeWork1_Task1;
+
+namespace PerimetrAndAreaOfSquare.UnitTests
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        [TestCase(7,49)]
+        [TestCase(1,1)]
+        public void SquareArea_CorrectValues_CalculateArea(int sideLength, int result)
+        {
+            var actual = Program.SquareArea(sideLength);
+            var expected = result;
+
+            Assert.AreEqual(expected, actual, "Error is found");
+
+            Console.WriteLine("Test 1 done"); 
+        }
+
+        [Test]
+        [TestCase(5,20)]
+        public void SquarePerimetr_CorrectValues_CalculatePerimetr(int sideLength, int result)
+        {
+            var actual = Program.SquarePerimetr(sideLength);
+            var expected = result;
+
+            Assert.AreEqual(expected, actual, "Error is found");
+
+            Console.WriteLine("Test 2 done");
+        }
+    }
+}
