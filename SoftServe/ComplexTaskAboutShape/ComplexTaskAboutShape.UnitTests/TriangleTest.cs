18a62da97b0212d946d163d7297ab19203ccbb07
DmutroKot <dmutrokot94@gmail.com>
2016-12-14 11:06:47 +0200

    Add Demo Task

---
+++ SoftServe/ComplexTaskAboutShape/ComplexTaskAboutShape.UnitTests/TriangleTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace ComplexTaskAboutShape.UnitTests
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        [Test]
+        public void Perimetr_GetPerimetr_Calculate()
+        {
+            Triangle triangle = new Triangle(new Point(-1, -1), new Point(-1, -4), new Point(-5, -1));
+
+            var actual = triangle.GetPerimetr();
+            var expected = 12;
+
+            Assert.AreEqual(expected, actual, "Test fails");
+        }
+
+        [Test]
+        public void Area_GetArea_Calculate()
+        {
+            Triangle triangle = new Triangle(new Point(-1, -1), new Point(-1, -4), new Point(-5, -1));
+
+            var actual = triangle.GetArea();
+            var expected = 6;
+
+            Assert.AreEqual(expected, actual, "Test fails");
+        }
+
+        [Test]
+        public void Triangle_CannotInitializeObject_ThrowException()
+        {
+            Assert.Throws<ArgumentException>(delegate { new Triangle(new Point(-1, -1), new Point(-1, -1), new Point(-5, -1)); });
+        }
+    }
+}
---
+++ SoftServe/ComplexTaskAboutShape/ComplexTaskAboutShape/Triangle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ComplexTaskAboutShape
+{
+    public class Triangle : IShape
+    {
+        private Point firstPoint;
+        private Point secondPoint;
+        private Point thirdPoint;
+
+        private double firstSideLength;
+        private double secondSideLength;
+        private double thirdSideLength;
+
+        public Triangle(Point first, Point second, Point third)
+        {
+            firstPoint = first;
+            secondPoint = second;
+            thirdPoint = third;
+
+            if (!IsValid)
+            {
+                throw new ArgumentException("Can't create triangle with that point", "thirdPoint");
+            }
+
+            DefineRange();
+        }
+
+        private void DefineRange()
+        {
+            firstSideLength = Math.Sqrt(Math.Pow((firstPoint.X - secondPoint.X), 2) + Math.Pow((firstPoint.Y - secondPoint.Y), 2));
+            secondSideLength = Math.Sqrt(Math.Pow((secondPoint.X - thirdPoint.X), 2) + Math.Pow((secondPoint.Y - thirdPoint.Y), 2));
+            thirdSideLength = Math.Sqrt(Math.Pow((thirdPoint.X - firstPoint.X), 2) + Math.Pow((thirdPoint.Y - firstPoint.Y), 2));
+        }
+
+        public double GetPerimetr()
+        {
+            return firstSideLength + secondSideLength + thirdSideLength;
+        }
+
+        public double GetArea()
+        {
+            double halfPerimetr = GetPerimetr() / 2;
+            return Math.Sqrt(halfPerimetr * (halfPerimetr - firstSideLength) * (halfPerimetr - secondSideLength) * (halfPerimetr - thirdSideLength));
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                return ((firstSideLength + secondSideLength) < thirdSideLength) ||
+                    ((secondSideLength + thirdSideLength) < firstSideLength) ||
+                    ((firstSideLength + thirdSideLength) < secondSideLength);
+            }
+        }
+
+        //public bool IsValid
+        //{
+        //    get
+        //    {
+        //        return (((firstPoint.X - secondPoint.X) != 0) || ((secondPoint.X - thirdPoint.X) != 0)) &&
+        //            (((firstPoint.Y - secondPoint.Y) != 0) || ((secondPoint.Y - thirdPoint.Y) != 0));
+        //    }
+        //}
+
+        public override string ToString()
+        {
+            var firstPointStr = string.Format("First point has coordinates x : {0}, y : {1}", firstPoint.X, firstPoint.Y);
+            var secondPointStr = string.Format("First point has coordinates x : {0}, y : {1}", secondPoint.X, secondPoint.Y);
+            var thirdPointStr = string.Format("First point has coordinates x : {0}, y : {1}", thirdPoint.X, thirdPoint.Y);
+
+            return firstPointStr + Environment.NewLine + secondPointStr + Environment.NewLine + thirdPointStr;
+        }
+    }
+}
