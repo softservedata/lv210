229102c52576d6fac607ecd258c122e5c85c801e
DmutroKot <dmutrokot94@gmail.com>
2016-12-19 12:24:46 +0200

    Add Homework5

---
+++ SoftServe/HomeWork5/WorkingWithList/WorkingWithList/ListInfo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkingWithList
+{
+    class ListInfo
+    {
+        ManipulationWithList list;
+        List<int> myCollection;
+        string[] inputData;
+
+        public ListInfo()
+        {
+            list = new ManipulationWithList();
+            myCollection = GetListFromConsole();
+        }
+
+        public void ElementPosition()
+        {
+            Console.Write("Input element for find position of it : ");
+            var elementPosition = int.Parse(Console.ReadLine());
+            Console.Write("Position(s) of element {0} is(are) : ", elementPosition);
+            list.GetElementPosition(myCollection, elementPosition);
+        }
+
+        public void RemoveFromList()
+        {
+            Console.Write("\nInput element which you want to remove from list (we remove all elements which greater than inputed element) : ");
+            var elementForRemove = int.Parse(Console.ReadLine());
+            Console.WriteLine("List after remove some element(s) : ");
+            list.RemoveElement(myCollection, elementForRemove);
+        }
+
+        public void InsertToList()
+        {
+            Console.WriteLine("Input separated by space elements what you want to insert : ");
+            inputData = Console.ReadLine().Split(' ');
+            var insertElements = ConvertFromStringToList(inputData);
+
+            Console.WriteLine("Input separated by space indexes where you want to insert (index can't be greater than list count): ");
+            inputData = Console.ReadLine().Split(' ');
+            var insertIndexes = ConvertFromStringToList(inputData);
+
+            Console.WriteLine("List after insert some element(s) : ");
+            myCollection = list.InsertElements(myCollection, insertElements, insertIndexes);
+        }
+
+        public void SortList()
+        {
+            Console.WriteLine("List after sort : ");
+            list.Sort(myCollection);
+        }
+
+        private static List<int> GetListFromConsole()
+        {
+            List<int> list = new List<int>();
+
+            Console.WriteLine("Input integer numbers separated by space : ");
+            string[] inputData = Console.ReadLine().Split(' ');
+
+            list = ConvertFromStringToList(inputData);
+
+            return list;
+        }
+        private static List<int> ConvertFromStringToList(string[] stringArray)
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                list.Add(int.Parse(stringArray[i]));
+            }
+
+            return list;
+        }
+    }
+}
---
+++ SoftServe/HomeWork5/WorkingWithList/WorkingWithList/ManipulationWithList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorkingWithList
+{
+    class ManipulationWithList
+    {
+        public void GetElementPosition(List<int> list, int element)
+        {
+            var foundIndex = list.Select((n, i) => new { number = n, index = i })
+                .Where(l => l.number == element)
+                .ToList();
+            foundIndex.ForEach(l => Console.Write(l.index + " "));
+        }
+
+        public void RemoveElement(List<int> list, int element)
+        {
+            list.RemoveAll(x => x >= element);
+
+            PrintList(list);
+        }
+
+        public List<int> InsertElements(List<int> list, List<int> element, List<int> index)
+        {
+            for (int i = 0; i < element.Count; i++)
+            {
+                list.Insert(index[i], element[i]);
+            }
+
+            PrintList(list);
+
+            return list;
+        }
+        public void Sort(List<int> list)
+        {
+            var array = list.ToArray();
+            Array.Sort(array);
+            var sortedList = array.ToList();
+
+            PrintList(sortedList);
+        }
+
+        public void PrintList(List<int> list)
+        {
+            list.ForEach(p => Console.Write(p.ToString() + " "));
+            Console.WriteLine();
+        }
+    }
+}
