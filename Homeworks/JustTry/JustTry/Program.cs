b9037918a22d62ce8a83b4f77396e577fb500746
mdemchuk <mari.de.la.krua@gmail.com>
2016-12-20 17:42:35 +0200

    Simple project with try/catch

---
+++ Homeworks/JustTry/JustTry/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace JustTry
+{
+    class Program
+    {
+        static void TestTryFinally()
+        {
+            Console.WriteLine("Code executed before try-finally.");
+            try
+            {
+                string str = Console.ReadLine();
+                Int32.Parse(str);
+                Console.WriteLine("Parsing was successful.");
+                return; // Exit from the current method
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Parsing failed!");
+            }
+            finally
+            {
+                Console.WriteLine(
+                  "This cleanup code is always executed.");
+            }
+            Console.WriteLine(
+              "This code is after the try-finally block.");
+        }
+
+        static void Main(string[] args)
+        {
+            TestTryFinally();
+            Console.ReadLine();
+        }
+    }
+}
