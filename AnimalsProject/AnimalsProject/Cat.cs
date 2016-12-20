5cb113e12109be2f6c9c3f81007f52cf3ee567dc
Orlann <orlann@ukr.net>
2016-12-20 16:22:19 +0200

    Interface

---
+++ AnimalsProject/AnimalsProject/Cat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsProject
+{
+    public class Cat : IAnimal
+    {
+        private string name;
+        private int food = 0;
+        public string Name { get { return name; } set { name = value; } }
+        public int Food { get { return food; } set { food = value; } }
+        public void Feed()
+        {
+            this.Food++;
+        }
+        public void Voice()
+        {
+            Console.WriteLine("miau-miau");
+        }
+    }
+}
