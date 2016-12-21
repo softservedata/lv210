8d16e5ca838470606094f2efcf50037a68cdc5c0
yharasym <yharasym@STUDENT>
2016-12-21 15:57:38 +0200

    Telerik Start

---
+++ UnitTestProjectTelerikFirst/TelerikFirstTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using ArtOfTest.WebAii.Core;
+
+namespace UnitTestProjectTelerikFirst
+{
+    [TestFixture]
+    public class TelerikFirstTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Manager manager;
+            Settings mySettings = new Settings();
+            //mySettings.Web.DefaultBrowser = BrowserType.FireFox;
+            //mySettings.Web.DefaultBrowser = BrowserType.InternetExplorer;
+            mySettings.Web.DefaultBrowser = BrowserType.Chrome;
+            manager = new Manager(mySettings);
+            manager.Start();
+            manager.LaunchNewBrowser();
+            manager.ActiveBrowser.NavigateTo("https://www.google.com.ua");
+            Thread.Sleep(2000);
+            //
+            manager.Dispose();
+            Console.WriteLine("done");
+        }
+    }
+}
