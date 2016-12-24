dcb2566da414471f2cd74f94d112eda4f4aa829a
mdemchuk <mari.de.la.krua@gmail.com>
2016-12-24 17:35:27 +0200

    something

---
+++ FilesTry/FilesTry/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Create("new_file.txt");
+        }
+    }
+}
