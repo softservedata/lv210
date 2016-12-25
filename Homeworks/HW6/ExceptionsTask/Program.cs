f2943f13982fb80faee9150353210c0fe9e7ed5b
mdemchuk <mari.de.la.krua@gmail.com>
2016-12-25 16:19:47 +0200

    homework6

---
+++ Homeworks/HW6/ExceptionsTask/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionsTask
+{
+    public class Program
+    {
+        public const int start = 1;
+        public const int end = 100;
+
+        public static bool IsValueInRange(int start, int end, int value)
+        {
+            return (value >= start) && (value <= end);
+        }
+
+        public static int ReadNumber(string inputedData)
+        {
+            int readVariable;
+            bool parseAtempt = int.TryParse(inputedData, out readVariable);
+            if (!parseAtempt)
+            {
+                throw new FormatException("Please, input positive <int> number");
+            }
+            if (!IsValueInRange(start, end, readVariable))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return readVariable;
+        }
+
+        static void Main(string[] args)
+        {
+            int numberCount = 10;
+            List<int> valuesList = new List<int>();
+            Console.WriteLine("Input 10 int number in range of [0, 100]");
+            try
+            {
+                for (int i = 0; i < numberCount; i++)
+                {
+                    valuesList.Add(ReadNumber(Console.ReadLine()));
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homeworks/HW6/ExceptionsTast.UnitTest/ExceptionsTaskUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using ExceptionsTask;
+
+namespace ExceptionsTast.UnitTest
+{
+    [TestFixture]
+    public class ExceptionsTaskUnitTest
+    {
+        public const int start = 1;
+        public const int end = 100;
+
+        [TestCase(-8, false)]
+        [TestCase(0, false)]
+        [TestCase(200, false)]
+        public void IsValueInRange_ShouldReturnFalse_WhenValueOutOfRange(int testValue, bool expectedResult)
+        {
+            bool actualResult = Program.IsValueInRange(start, end, testValue);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(1, true)]
+        [TestCase(100, true)]
+        [TestCase(50, true)]
+        public void IsValueInRange_ShouldReturnTrue_WhenValueIsInRange(int testValue, bool expectedResult)
+        {
+            bool actualResult = Program.IsValueInRange(start, end, testValue);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("string")]
+        public void ReadNumber_ShouldThowException_WhenValuesIsInvalid(string inputedData)
+        {
+            Assert.Throws<FormatException>(() => Program.ReadNumber(inputedData));
+        }
+
+        [TestCase("-8")]
+        [TestCase("0")]
+        [TestCase("200")]
+        public void ReadNumber_ShouldThowException_WhenValuesIsOutOfRange(string inputedData)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Program.ReadNumber(inputedData));
+        }
+    }
+}
