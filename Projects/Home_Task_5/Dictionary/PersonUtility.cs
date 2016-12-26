97c9809311a64158a9a526269665fb7cddece9ce
rostyslav.zhalivtsiv <rostyslav.zhalivtsiv@gmail.com>
2016-12-26 22:54:03 +0200

    edit

---
+++ Projects/Home_Task_5/Dictionary/PersonUtility.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public static class PersonUtility
+    {
+        /// <summary>
+        /// Check if read value from console is unsigned integer
+        /// </summary>
+        /// <param name="consoleString">String read from console</param>
+        /// <returns></returns>
+        private static bool IsUnsignedInteger(string consoleString)
+        {
+            uint integerValue;
+            if (UInt32.TryParse(consoleString, out integerValue))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Throw exeption if in collection exist item with the same key
+        /// </summary>
+        /// <param name="idCollection">Collection which should be checked</param>
+        /// <param name="id">Key (id) which should be checked for duplication</param>
+        private static void CheckDublicatesForId(ICollection<uint> idCollection, uint id)
+        {
+            if (idCollection.Contains(id))
+                throw new ArgumentException("\nPerson with the same key has already been added.");
+        }
+
+        /// <summary>
+        /// If all conditions are passed, return valid ID, if not - throw exeption message
+        /// </summary>
+        /// <param name="idRead">ID read from console</param>
+        /// <param name="idCollection">Collection with ID keys</param>
+        /// <returns></returns>
+        public static uint GetID(string idRead, ICollection<uint> idCollection)
+        {
+            uint validID;
+
+            if (IsUnsignedInteger(idRead))
+            {
+                validID = UInt32.Parse(idRead);
+                CheckDublicatesForId(idCollection, validID);
+                return validID;
+            }
+
+            throw new FormatException($"\n{idRead} - is not unsigned integer!");
+        }
+
+        /// <summary>
+        /// For input name of person
+        /// </summary>
+        /// <returns>Name read from Console</returns>
+        public static string InputName()
+        {
+            Console.Write("Enter the name: ");
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// For input ID of person
+        /// </summary>
+        /// <returns>(string)ID read from Console</returns>
+        public static string InputId()
+        {
+            Console.Write("Enter ID: ");
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Prints all members from dictionary
+        /// </summary>
+        /// <param name="dictionary">Dictionary which would like to display</param>
+        public static void Display(Dictionary<uint, string> dictionary)
+        {
+            foreach (KeyValuePair<uint, string> kvp in dictionary)
+            {
+                Console.WriteLine("ID:[{0}] ==> {1}", kvp.Key, kvp.Value);
+            }
+        }
+
+
+        /// <summary>
+        /// Search name of person by its ID
+        /// </summary>
+        /// <param name="dictionary">Collections in which contains persons</param>
+        /// <returns>If ID matches returns name of person</returns>
+        public static string GetValueByID(Dictionary<uint, string> dictionary)
+        {
+            uint id;
+            UInt32.TryParse(InputId(), out id);
+
+            string value;
+            if (dictionary.TryGetValue(id, out value))
+                return value;
+
+            return "Fail! Can't find this ID.";
+        }
+    }
+}
--- Projects/Home_Task_5/Dictionary/Program.cs
+++ Projects/Home_Task_5/Dictionary/Program.cs
@@ -17,52 +17,34 @@ namespace Dictionary
         static void Main(string[] args)
         {
             Dictionary<uint, string> persons = new Dictionary<uint, string>();
+            int numberOfPersons = 2;
 
-            int numberOfPersons = 7;
-
-            for (int i = 0; i < numberOfPersons; i++)
+            try
             {
-                Console.Write("Enter the name: ");
-                Person person = new Person(Console.ReadLine());
-                persons.Add(person.ID, person.Name);
+                for (int i = 0; i < numberOfPersons; i++)
+                {
+                    string name = PersonUtility.InputName();
+                    uint id = PersonUtility.GetID(PersonUtility.InputId(), persons.Keys);
+
+                    persons.Add(id, name);
+                }
             }
 
-            Display(persons);
-
-            Console.WriteLine("Enter ID to find: ");
-            string searchingID = Console.ReadLine();
-
-            Console.WriteLine(GetValueByID(persons, searchingID));
-        }
-
-        /// <summary>
-        /// Prints all members from Dictionary
-        /// </summary>
-        /// <param name="dictionary">Dictionary which would like to display</param>
-        static void Display(Dictionary<uint, string> dictionary)
-        {
-            foreach (KeyValuePair<uint, string> kvp in dictionary)
+            catch (FormatException ex)
             {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+                Console.Error.WriteLine(ex.Message);
             }
-        }
 
-        /// <summary>
-        /// By given ID try get relevant value from Dictionary
-        /// </summary>
-        /// <param name="dictionary">Dictionary which will search for</param>
-        /// <param name="readFromConsoleID">Given ID by User</param>
-        /// <returns></returns>
-        static string GetValueByID(Dictionary<uint, string> dictionary, string readFromConsoleID)
-        {
-            uint id;
-            UInt32.TryParse(readFromConsoleID, out id);
+            catch (ArgumentException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
 
-            string value;
-            if (dictionary.TryGetValue(id, out value))
-                return value;
+            Console.WriteLine("\n----List of Persons----");
+            PersonUtility.Display(persons);
 
-            return "Fail! Can't find this ID.";
+            Console.WriteLine("\n----Find Person in List----");
+            Console.WriteLine(PersonUtility.GetValueByID(persons));
         }
     }
 }
