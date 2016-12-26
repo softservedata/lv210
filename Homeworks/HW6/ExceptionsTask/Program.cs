d4ff1e479985bbd4af99ae2612c835f37db4dcc7
mdemchuk <mari.de.la.krua@gmail.com>
2016-12-26 22:03:42 +0200

    add summary

--- Homeworks/HW6/ExceptionsTask/Program.cs
+++ Homeworks/HW6/ExceptionsTask/Program.cs
@@ -8,15 +8,29 @@ namespace ExceptionsTask
         public const int start = 1;
         public const int end = 100;
 
+        /// <summary>
+        /// Method checks if integer number in the range [start...end] 
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static bool IsValueInRange(int start, int end, int value)
         {
             return (value >= start) && (value <= end);
         }
 
+        /// <summary>
+        /// Method read variable
+        /// </summary>
+        /// <param name="inputedData"></param>
+        /// <returns>If variable is valid and belong to interval method returns variable otherwise 
+        /// method thows exceptions</returns>
         public static int ReadNumber(string inputedData)
         {
             int readVariable;
             bool parseAtempt = int.TryParse(inputedData, out readVariable);
+
             if (!parseAtempt)
             {
                 throw new FormatException("Please, input positive <int> number");
