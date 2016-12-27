7af2e5c887cf295fc4a1031c3ed81cffdc2a5c27
mdemchuk <mari.de.la.krua@gmail.com>
2016-12-27 14:40:09 +0200

    work with telerik

--- Telerik/TelerikFirstTry/TelerikFirstTry/UnitTest1.cs
+++ Telerik/TelerikFirstTry/TelerikFirstTry/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
 using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.Controls.HtmlControls;
 
 namespace TelerikFirstTry
 {
@@ -12,10 +13,15 @@ namespace TelerikFirstTry
         public void TestMethod()
         {
             Settings mySettings = new Settings();
-            mySettings.Web.DefaultBrowser = BrowserType.FireFox;
+            mySettings.Web.DefaultBrowser = BrowserType.Chrome;
             manager = new Manager(mySettings);
             manager.Start();
             manager.LaunchNewBrowser();
+            manager.ActiveBrowser.NavigateTo("https://www.google.com.ua/");
+            HtmlAnchor signin = manager.ActiveBrowser.Find.ById<HtmlAnchor>("gb_70");
+            //signin.MouseHover();
+            //signin.Focus();
+            signin.Click();
         }
     }
 }
