ce9aaf80ac4716504e6c69b78254eeaa9109fa90
orlann <orlann30@gmail.com>
2017-01-02 00:39:53 +0200

    changes

---
+++ Homework1/HW1_2 (PersonInfo)/HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define string variable name and integer value age
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Yor are {0} and you are {1} years old.", name, age);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework1/Hw1_1 (SquareArea)/HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сalculate area and perimetr of square with length a
+            Console.WriteLine("Enter the length of the square: ");
+            String a_s = Console.ReadLine();
+            int a = Convert.ToInt32(a_s);
+            Console.WriteLine("Area of square = {0}", a * a);
+            Console.WriteLine("Perimeter of square = {0}", 4 * a);
+            Console.ReadKey();
+        }
+    }
+}
