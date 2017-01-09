d8624290c604e91d76ca19a0fd2d03519bccd46b
mdemchuk <mari.de.la.krua@gmail.com>
2017-01-09 19:09:05 +0200

    hw8

---
+++ Homeworks/HW8/HW8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace LinQTasks
+{
+    class Program
+    {
+        //Read data
+        public static string[] ReadData(string fileName)
+        {
+            return File.ReadAllLines(fileName);
+        }
+
+        static void Main()
+        {
+            var data = ReadData("hp1.txt");
+            FileOperations fileData = new FileOperations();
+
+            fileData.StringCount(data);
+            fileData.LongestString(data);
+            fileData.ShortestString(data);
+            fileData.SearchString(data, "wand");
+        }
+    }
+}
