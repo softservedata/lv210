f935dbb25a79db2a03068d8a377a146d88954813
mdemchuk <mari.de.la.krua@gmail.com>
2017-01-10 20:21:49 +0200

    languagepage

--- Wow/Pages/LanguagesPage.cs
+++ Wow/Pages/LanguagesPage.cs
@@ -1,31 +1,25 @@
-﻿using ArtOfTest.WebAii.Core;
-using ArtOfTest.WebAii.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ArtOfTest.WebAii.Core;
 using ArtOfTest.WebAii.Controls.HtmlControls;
-using Wow.Data;
 
 namespace Wow.Pages
 {
     class LanguagesPage : HeadPage
     {
-        // Fields
         private Manager manager;
-        private string language;
 
-        // get Data
-        //html TODO
-        public Element LanguagesLabel { get; private set; }
-        public HtmlSelect SelectLanguageList { get; private set; }
-        public HtmlSpan Add { get; private set; }
-        public Element Language { get; private set; }
+        public HtmlElement LanguagesLabel { get; private set; }
+        public HtmlSelect SelectLanguage { get; private set; }
+        public HtmlSpan AddLanguage { get; private set; }
+        public 
 
-        //Constuctor
         public LanguagesPage(Manager manager) : base(manager)
         {
-            this.manager = manager;
-            LanguagesLabel = manager.ActiveBrowser.Find.ByContent("Languages");
-            SelectLanguageList = manager.ActiveBrowser.Find.ByAttributes<HtmlSelect>("ng-options=language.Name for language in languageList");
-            Add = manager.ActiveBrowser.Find.ByAttributes<HtmlSpan>("class=input-group-btn");
-            Language = manager.ActiveBrowser.Find.ByContent(language);
         }
     }
 }
