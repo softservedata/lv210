2e455d1cd8985658e648c765e20e4517682a7303
roman_lebedovych <romanlebedovych21@gmail.com>
2017-01-10 13:51:22 +0200

    add homework 8

---
+++ homeworks/Homework_8/Homework_8/Square.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8
+{
+   public class Square : Shape
+    {
+        public double Side { get; set; }
+        public Square(string name, double side) : base(name)
+        {
+            Side = side;
+        }
+
+        public override double Area()
+        {
+            return Side * Side;
+        }
+
+        public override double Perimeter()
+        {
+            return 4 * Side;
+        }
+
+        public override string ToString()
+        {
+            return Name + " type: square. Side length = " + Side + ". Perimeter = " + this.Perimeter() + ". Area = " +
+                   this.Area();
+        }
+    }
+}
