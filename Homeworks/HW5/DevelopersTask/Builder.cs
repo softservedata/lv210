b29f5d895251711b9bf9dd5e2e0b0a068caddf5d
mdemchuk <mari.de.la.krua@gmail.com>
2017-01-12 11:36:13 +0200

    code refactoring

---
+++ Homeworks/HW5/DevelopersTask/Builder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DevelopersTask
+{
+    public class Builder : IDeveloper
+    {
+        private string firstName;
+        private string lastName;
+        private string tool;
+
+        public Builder()
+        {
+        }
+
+        public Builder(string firstName, string lastName, string tool)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.tool = tool;
+        }
+
+        public string Tool
+        {
+            get
+            {
+                return tool;
+            }
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("Builder {0} {1} was created", firstName, lastName);
+        }
+
+        public void Destroy()
+        {
+            firstName = null;
+            lastName = null;
+            tool = null;
+        }
+    }
+}
---
+++ Homeworks/HW5/DevelopersTask/Programmer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DevelopersTask
+{
+    class Programmer : IDeveloper
+    {
+        private string firstName;
+        private string lastName;
+        private string tool;
+
+        public Programmer()
+        {
+        }
+
+        public Programmer(string firstName, string lastName, string tool)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.tool = tool;
+        }
+
+        public string Tool
+        {
+            get
+            {
+                return tool;
+            }
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("Programmer {0} {1} was created", firstName, lastName);
+        }
+
+        public void Destroy()
+        {
+            firstName = null;
+            lastName = null;
+            tool = null;
+        }
+    }
+}
