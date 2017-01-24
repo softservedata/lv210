4b52df7f28a10ad441b05d537e670777489dd321
DmutroKot <dmutrokot94@gmail.com>
2017-01-24 13:37:08 +0200

    Add HomeWork10

---
+++ SoftServe/HomeWork10/BinarySerialization/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BinarySerialization
+{
+    [Serializable]
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+        [DataMember]
+        public string Country { get; set; }
+        [DataMember]
+        public string City { get; set; }
+    }
+}
---
+++ SoftServe/HomeWork10/BinarySerialization/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using System.Xml.Serialization;
+
+namespace BinarySerialization
+{
+    class Program
+    {
+        public static string GetPath(string fileName)
+        {
+            var directory = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()));
+            return Path.Combine(directory, fileName);
+        }
+
+        private static void BinarySerialization(string fileName, Person[] person)
+        {
+            IFormatter formatter = new BinaryFormatter();
+
+            using (Stream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                formatter.Serialize(stream, person);
+            }
+        }
+
+        private static void XmlSerialization(string fileName, Person[] person)
+        {
+            XmlSerializer formatterXml = new XmlSerializer(typeof(Person[]));
+
+            using (Stream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                formatterXml.Serialize(stream, person);
+            }
+        }
+
+        private static void JsonSerialization(string fileName, Person[] person)
+        {
+            DataContractJsonSerializer formatterJson = new DataContractJsonSerializer(typeof(Person[]));
+
+            using (Stream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                formatterJson.WriteObject(stream, person);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                FirstName = "Jonh",
+                LastName = "Smith",
+                Age = 33,
+                Country = "UK",
+                City = "London"
+            };
+
+            Person person1 = new Person
+            {
+                FirstName = "Massimo",
+                LastName = "Peruzzi",
+                Age = 28,
+                Country = "Italy",
+                City = "Torino"
+            };
+
+            Person[] persons = new Person[] { person, person1 };
+
+            var binFilePath = GetPath("Persons.bin");
+            var xmlFilePath = GetPath("Persons.xml");
+            var jsonFilePath = GetPath("Persons.json");
+
+            BinarySerialization(binFilePath, persons);
+            XmlSerialization(xmlFilePath, persons);
+            JsonSerialization(jsonFilePath, persons);
+
+            Console.ReadKey();
+        }
+    }
+}
